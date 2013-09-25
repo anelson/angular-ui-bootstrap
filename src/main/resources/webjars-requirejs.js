3c968540c723507289d331d8ebf5aa228b80b390
Adam Nelson <anelson@apocryph.org>
2013-09-25 02:08:02 -0400

    Add shim for 'ui-bootstrap-tpls' so it also depends on angular

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -3,6 +3,7 @@
 // Ensure any request for this webjar brings in dependencies.
 requirejs.config({
     shim: {
-        'ui-bootstrap': [ 'webjars!angular.js', 'webjars!bootstrap.js' ]
+        'ui-bootstrap': [ 'webjars!angular.js', 'webjars!bootstrap.js' ],
+        'ui-bootstrap-tpl': [ 'webjars!angular.js', 'webjars!bootstrap.js' ]
     }
 });
